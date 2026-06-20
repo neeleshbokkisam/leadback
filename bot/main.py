beb1c93c888f0c6bea7c0366473f316272a39b1d
neeleshbokkisam <neeleshbokkisam@berkeley.edu>
2026-06-19 20:04:31 -0400

    discord listener

---
+++ bot/main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+CHANNEL_ID = int(os.getenv("FEEDBACK_CHANNEL_ID", 0))
+
+
+@client.event
+async def on_ready():
+    print(f"connected as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.id != CHANNEL_ID:
+        return
+
+
+if __name__ == "__main__":
+    client.run(os.getenv("DISCORD_TOKEN"))
